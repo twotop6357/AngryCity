20d0f3099169602156e4da7eca79b412f2b19a8f
twotop6357 <twotop6357@gmail.com>
2024-05-27 15:58:44 +0900

    Add Map, Player, UI
    무료 에셋을 통해 맵 추가, 플레이어 점프, 이동, 화면 움직임, 달리기 기능 추가, 스태미너, 체력 추가, 스태미너가 모두 감소하면 달리기 강제 종료 기능 추가

---
+++ Assets/Scripts/UI/Condition.cs
@@ -0,0 +1,40 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Condition : MonoBehaviour
+{
+    public float curValue;
+    public float startValue;
+    public float maxValue;
+    public float passiveValue;
+    public Image uiBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curValue = startValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        uiBar.fillAmount = GetPercentage();
+    }
+
+    float GetPercentage()
+    {
+        return curValue / maxValue;
+    }
+
+    public void Add(float value)
+    {
+        curValue = Mathf.Min(curValue + value, maxValue);
+    }
+
+    public void Subtract(float value)
+    {
+        Debug.Log("±ð¿´Siu");
+        curValue = Mathf.Max(curValue - value, 0);
+    }
+}
---
+++ Assets/Scripts/UI/UICondition.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UICondition : MonoBehaviour
+{
+    public Condition health;
+    public Condition stamina;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CharacterManager.Instance.Player.condition.uiCondition = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
