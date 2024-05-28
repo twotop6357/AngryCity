b36c067dc04fb3b1639aeeb3f972447ab20cc1d2
twotop6357 <twotop6357@gmail.com>
2024-05-28 19:15:37 +0900

    Add GameOver
    게임오버 조건을 추가하였습니다.

--- Assets/Scripts/Player/PlayerCondition.cs
+++ Assets/Scripts/Player/PlayerCondition.cs
@@ -1,16 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerCondition : MonoBehaviour
 {
     public UICondition uiCondition;
+    [Header("GameOver")]
+    public GameObject gameOverPanel;
+    public TextMeshProUGUI gameOverText;
+    private float sec;
+    private int min;
+
 
     Condition health { get { return uiCondition.health; } }
     Condition stamina {  get { return uiCondition.stamina; } }
 
     void Update()
     {
+        sec += Time.deltaTime;
+        if (sec >= 60f)
+        {
+            min += 1;
+            sec = 0;
+        }
+
         if (CharacterManager.Instance.Player.controller.isSprint == false)
         {
             stamina.Add(stamina.passiveValue * Time.deltaTime);
@@ -29,6 +43,9 @@ public class PlayerCondition : MonoBehaviour
 
     public void Die()
     {
-        Debug.Log("죽었다!");
+        gameOverPanel.SetActive(true);
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.Confined;
+        gameOverText.text = string.Format("{0}분 {1:D2}초간 노력했지만 끝내 차갑게 식었습니다...", min, (int)sec);
     }
 }
